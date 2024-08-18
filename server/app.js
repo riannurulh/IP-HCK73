df57271e588393aa3d6af5758deb3895194adf51
R <r@Amandas-MacBook-Air.local>
2024-08-18 22:56:49 +0700

    test commit

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV !== "production") {
     require('dotenv').config()
 }
-
+// j
 const express = require('express')
 const cors = require('cors')
 const router = require('./routers/router')
